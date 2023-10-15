827cf428208411a1ae2623cd30738fa54fff7b7b
Klim Medvedev <dater01@mail.ru>
2023-10-15 22:17:16 +0300

    -

--- scripts/postapi.js
+++ scripts/postapi.js
@@ -1,16 +1,16 @@
 import http from 'k6/http';
-import {sleep} from 'k6';
+import { sleep } from 'k6';
 
 export const options = {
     stages: [
-        {duration: '300s', target: 100 }
+        { duration: '5m', target: 150 }
     ],
 };
 
 export default function () {
-    const url='http://host.docker.internal:8080/post-message';
+    const url = 'http://host.docker.internal:8080/post-message';
 
-    const msg_id = Math.floor(Math.random() * 1000).toString();
+    const msg_id = Math.floor(Math.random() * 100000).toString();
 
     const payload = JSON.stringify({
         msg_id: msg_id,
@@ -18,15 +18,14 @@ export default function () {
 
     const params = {
         headers: {
-            'Content-Type' : 'application/json'
-        }
+            'Content-Type': 'application/json',
+        },
     };
 
     const res = http.post(url, payload, params);
 
     console.log(`Ответ сервера: ${res.status} - ${res.body}`);
 
-
-    sleep(1);
-
+    const sleepDuration = 1 / 0.5 * 1000;
+    sleep(sleepDuration);
 }
